e1e4e20fd5fd72e4a307b13ba841b3f046a6a3a6
altindalorcun <altindalorcun@gmail.com>
2024-07-24 22:01:07 +0300

    Add mutations

--- src/main/java/dev/altindal/graphql_client/controller/UserController.java
+++ src/main/java/dev/altindal/graphql_client/controller/UserController.java
@@ -1,13 +1,12 @@
 package dev.altindal.graphql_client.controller;
 
+import dev.altindal.graphql_client.dto.UserCreateRequest;
 import dev.altindal.graphql_client.dto.UserResponse;
+import dev.altindal.graphql_client.dto.UserUpdateRequest;
 import dev.altindal.graphql_client.service.UserGraphqlRequester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -30,5 +29,20 @@ public class UserController {
     @GetMapping("/user/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
         return new ResponseEntity<>(service.getUserById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserCreateRequest request) {
+        return new ResponseEntity<>(service.createUser(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<UserResponse> updateUser(@RequestBody UserUpdateRequest request) {
+        return new ResponseEntity<>(service.updateUser(request), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<Boolean> deleteUser(@PathVariable UUID id) {
+        return new ResponseEntity<>(service.deleteUser(id), HttpStatus.OK);
     }
 }
--- src/main/java/dev/altindal/graphql_client/service/UserGraphqlRequester.java
+++ src/main/java/dev/altindal/graphql_client/service/UserGraphqlRequester.java
@@ -1,6 +1,8 @@
 package dev.altindal.graphql_client.service;
 
+import dev.altindal.graphql_client.dto.UserCreateRequest;
 import dev.altindal.graphql_client.dto.UserResponse;
+import dev.altindal.graphql_client.dto.UserUpdateRequest;
 import org.springframework.graphql.client.HttpGraphQlClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -70,6 +72,92 @@ public class UserGraphqlRequester {
                 .variables(variables)
                 .retrieve("getUserById")
                 .toEntity(UserResponse.class)
+                .block();
+    }
+
+    public UserResponse createUser(UserCreateRequest request) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("username", request.username());
+        variables.put("password", request.password());
+        variables.put("firstName", request.firstName());
+        variables.put("lastName", request.lastName());
+        variables.put("mail", request.mail());
+        variables.put("role", request.role());
+        Map<String, Object> input = new HashMap<>();
+        input.put("input", variables);
+
+        //language=graphql
+        String mutation = """
+                mutation($input : UserCreateRequest!) {
+                    createUser(userCreateRequest: $input) {
+                        id
+                        createdAt
+                        updatedAt
+                        username
+                        firstName
+                        lastName
+                        mail
+                        role
+                    }
+                }
+                """;
+
+        return graphQlClient.document(mutation)
+                .variables(input)
+                .retrieve("createUser")
+                .toEntity(UserResponse.class)
+                .block();
+    }
+
+    public UserResponse updateUser(UserUpdateRequest request) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("id", request.id());
+        variables.put("password", request.password());
+        variables.put("firstName", request.firstName());
+        variables.put("lastName", request.lastName());
+        variables.put("mail", request.mail());
+        variables.put("role", request.role());
+        Map<String, Object> input = new HashMap<>();
+        input.put("input", variables);
+
+        //language=graphql
+        String mutation = """
+                mutation($input : UserUpdateRequest!) {
+                    updateUser(userUpdateRequest: $input) {
+                        id
+                        createdAt
+                        updatedAt
+                        username
+                        firstName
+                        lastName
+                        mail
+                        role
+                    }
+                }
+                """;
+
+        return graphQlClient.document(mutation)
+                .variables(input)
+                .retrieve("updateUser")
+                .toEntity(UserResponse.class)
+                .block();
+    }
+
+    public Boolean deleteUser(UUID id) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("id", id);
+
+        //language=graphql
+        String mutation = """
+                mutation ($id : UUID!) {
+                    deleteUser(id : $id)
+                }
+                """;
+
+        return graphQlClient.document(mutation)
+                .variables(variables)
+                .retrieve("deleteUser")
+                .toEntity(Boolean.class)
                 .block();
     }
 }
